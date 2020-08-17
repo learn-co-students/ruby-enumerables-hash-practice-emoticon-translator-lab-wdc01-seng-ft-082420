782e6eef9916771eb118d9bddb4670c34bb57d16
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-17 21:39:46 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,13 @@
+require 'yaml'
+require 'pry'
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(file_path)
+ emoticon_hash= YAML.load_file(file_path)
+    binding.pry
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(emoticon_hash)
   # code goes here
 end
 
